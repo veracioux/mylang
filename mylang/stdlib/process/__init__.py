65052bf2bcf65589ace365bc3efcfb63d05e4153
Haris Gušić <harisgusic.dev@gmail.com>
2025-09-18 22:11:43 +0200

    feat: Add process module with run function

---
+++ mylang/stdlib/process/__init__.py
@@ -0,0 +1,22 @@
+import sys
+import subprocess
+from ..core._utils import FunctionAsClass, expose_obj_attr, function_defined_as_class, Special
+from ..core import Object, String
+
+
+@function_defined_as_class
+class run(Object, FunctionAsClass):
+    _CLASSCALL_SHOULD_RECEIVE_NEW_STACK_FRAME = False
+
+    @Special._m_classcall_
+    @classmethod
+    def _m_classcall_(cls, args):
+        assert args.is_positional_only(), "Only positional arguments are allowed"
+        argv = args[:]
+        assert all(
+            isinstance(arg, String) for arg in argv
+        ), "All arguments must be strings"
+        subprocess.run(tuple(str(arg) for arg in argv), check=True)
+
+
+expose_obj_attr(sys.modules[__name__], "run")
